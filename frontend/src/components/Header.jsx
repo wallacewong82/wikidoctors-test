27f1c80b7801d6a4d3880830a21401e2e713958c
[Wallace <wwong63@gatech.edu>
2024-01-07 12:27:13 +0800

    test commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Nav, Navbar, Container, NavDropdown, Text} from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar collapseOnSelect>
+        <Container>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
